e5cad7a1885dae0075bd03ae695c3e22d552e77d
Tomas Pugente <pugentetomas@gmail.com>
2025-05-27 16:43:51 -0300

    base

--- src/main/java/com/grupo12/SistemaTurnosApplication.java
+++ src/main/java/com/grupo12/SistemaTurnosApplication.java
@@ -1,4 +1,4 @@
-package com.grupo12.SistemaTurnos;
+package com.grupo12;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/grupo12/helper/ViewRouteHelper.java
@@ -0,0 +1,9 @@
+package com.grupo12.helper;
+
+public class ViewRouteHelper {
+
+    public static final String INDEX = "index";
+    public static final String LOGIN = "login";
+    public static final String REGISTER = "register";
+    public static final String FORGOT_PASSWORD= "forgotPassword";
+}
