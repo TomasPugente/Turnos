2d82cdd577cacda6a96173b83d13f3c4ecdfaf7d
Tomas Pugente <pugentetomas@gmail.com>
2025-06-05 01:35:44 -0300

    ajustes

--- src/main/java/com/grupo12/repositories/IEmployeeRepository.java
+++ src/main/java/com/grupo12/repositories/IEmployeeRepository.java
@@ -4,9 +4,11 @@ import java.io.Serializable;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.grupo12.entities.Employee;
 
+@Repository("employeeRepository")
 public interface IEmployeeRepository extends JpaRepository<Employee, Serializable> {
     boolean existsByDni(String dni);
 
--- src/main/java/com/grupo12/services/IUserService.java
+++ src/main/java/com/grupo12/services/IUserService.java
@@ -1,5 +1,8 @@
 package com.grupo12.services;
 
+import org.springframework.stereotype.Service;
+
+@Service("userService")
 public class IUserService {
 
 }
