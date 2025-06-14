2a90317136be08403ee3902280ebb8b73ddd95e8
Alan <alanretamar99@outlook.com.ar>
2025-06-14 20:47:05 -0300

    Agrego casos de uso de Client

---
+++ src/main/java/com/grupo12/repositories/IContactRepository.java
@@ -0,0 +1,14 @@
+package com.grupo12.repositories;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.grupo12.entities.Contact;
+
+@Repository
+public interface IContactRepository extends JpaRepository<Contact, Long> {
+
+	Optional<Contact> findByIdContact(Integer id);
+}
