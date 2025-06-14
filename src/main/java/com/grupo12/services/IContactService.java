bd40d988ad948ae51ec061bbfc547cb4933aaf8e
Alan <alanretamar99@outlook.com.ar>
2025-06-14 20:47:05 -0300

    Agrego casos de uso de Client

---
+++ src/main/java/com/grupo12/services/IContactService.java
@@ -0,0 +1,12 @@
+package com.grupo12.services;
+
+import java.util.Optional;
+import com.grupo12.entities.Contact;
+
+public interface IContactService {
+	Contact insertOrUpdate(Contact contact);
+    Contact save(Contact contact);
+	Optional<Contact> getById(Integer id);
+	
+}
+
---
+++ src/main/java/com/grupo12/services/implementation/ContactService.java
@@ -0,0 +1,36 @@
+package com.grupo12.services.implementation;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.grupo12.entities.Contact;
+import com.grupo12.repositories.IContactRepository;
+import com.grupo12.services.IContactService;
+
+import jakarta.transaction.Transactional;
+
+@Service("contactService")
+public class ContactService implements IContactService {
+
+    @Autowired
+    private IContactRepository contactRepository;
+
+    @Override
+    @Transactional
+    public Contact insertOrUpdate(Contact contact) {
+        // Si tiene ID, es una actualización; si no, es una inserción
+        return contactRepository.save(contact);
+    }
+    
+    @Override
+    public Optional<Contact> getById(Integer id) {
+        return contactRepository.findByIdContact(id);
+    }
+
+    @Override
+    public Contact save(Contact contact) {
+        return contactRepository.save(contact);
+    }
+}
