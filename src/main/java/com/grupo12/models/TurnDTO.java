4b8cd3ae0a19ff06c2fbf7933d8140546a6e3d68
maryse90 <106642662+maryse90@users.noreply.github.com>
2025-06-19 00:38:29 -0300

    Add files via upload

--- src/main/java/com/grupo12/models/TurnDTO.java
+++ src/main/java/com/grupo12/models/TurnDTO.java
@@ -2,31 +2,87 @@ package com.grupo12.models;
 
 import java.util.Date;
 
+import com.grupo12.entities.Contact;
+
+import java.time.LocalDateTime;
+import java.time.LocalDate;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 
 @Getter
 @Setter
+@Data
 @NoArgsConstructor
+@AllArgsConstructor //Ya genera el constructor de todos los argumentos
 public class TurnDTO {
 
-	private int idTurn;
+    private int idTurn;
+    private String state;
+    private boolean active;
+    private String observation;	
+    private ClientDTO client;
+    private EmployeeDTO employee;
+    private Date date;
+    private Integer idServicio;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String status; //Representa el enum como String
+    private LocalDateTime creationTime;
+	
+    
+    //Campos para el cliente
+    private Integer clientIdPerson;
+    private String clientName;
+    private String clientDni;
+    private LocalDate clientDateOfBirth;
+    private String clientPassword;
+    private String clientEmail;
+    
+    //Campos para el empleado
+    private Integer employeeIdPerson;
+    private String employeeName;
+    private String employeeDni;
+    private LocalDate employeeDateOfBirth;
+    private String employeePassword;
+    private String employeeEmail;
+    private String employeeCuit;
+    private LocalDate employeeEntryDate;
+    
+    //Campos para el servicio
+    private String serviceName;
+    private int serviceId;
+    
+    public int getEmployeeId() {
+        return employeeIdPerson;
+    }
 
-	private String state;
+    public void setEmployeeId(int employeeId) {
+        this.employeeIdPerson = employeeId;
+    }
 
-	private boolean active;
+    public int getServiceId() {
+        return serviceId;
+    }
 
-	private String observation;
+    public void setServiceId(int serviceId) {
+        this.serviceId = serviceId;
+    }
+    /*public Employee(Integer idPerson, String name, String dni, LocalDate dateOfBirth, String password, Contact contact,
+			     LocalDate entryDate, String cuit) {
+		super(idPerson, name, dni, dateOfBirth, password, contact);
+		this.entryDate = entryDate;
+		this.cuit = cuit;
+	}
+    */
+    
+    
 
-	private ClientDTO client;
-
-	private EmployeeDTO employee;
-
-	private Date date;
-
-	public TurnDTO(int idTurn, String state, boolean active, String observation, ClientDTO client, EmployeeDTO employee,
-			Date date) {
+	/*public TurnDTO(int idTurn, String state, boolean active, String observation, ClientDTO client, EmployeeDTO employee, 
+			Date date, int idServicio, LocalDateTime startTime, LocalDateTime endTime, String status, LocalDateTime creationTime) {
 		super();
 		this.idTurn = idTurn;
 		this.state = state;
@@ -35,6 +91,14 @@ public class TurnDTO {
 		this.client = client;
 		this.employee = employee;
 		this.date = date;
-	}
-
+		this.idServicio = idServicio;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.status = status;
+		this.creationTime = creationTime;
+	}*/
+    
+    
+    
+    
 }
