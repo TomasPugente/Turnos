1b875080bab0715d17300cbb4df8a69c06316648
maryse90 <106642662+maryse90@users.noreply.github.com>
2025-06-19 00:55:03 -0300

    Add files via upload

---
+++ src/test/java/com/grupo12/SistemaTurnos/controllers/TurnControllerTest.java
@@ -0,0 +1,46 @@
+package com.grupo12.controllers;
+import com.grupo12.controllers.TurnController;
+import com.grupo12.models.TurnDTO;
+import com.grupo12.services.ITurnService;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest(controllers = TurnController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class TurnControllerTest {
+		@Autowired
+	    private MockMvc mockMvc;
+
+	    @MockBean
+	    private ITurnService turnService;
+
+	    @Test
+	    public void callNextTurn_shouldReturnTurnDTO() throws Exception {
+	    	// Arrange: crear TurnDTO simulado
+	        TurnDTO mockTurn = new TurnDTO();
+	        mockTurn.setIdTurn(1);
+	        mockTurn.setStatus("EN_ATENCION");
+
+	        int empleadoId = 5;
+
+	        when(turnService.callNextTurnForEmployee(empleadoId)).thenReturn(mockTurn);
+
+	        // Act & Assert: simular GET /turns/llamar/5
+	        mockMvc.perform(get("/turns/llamar/{id}", empleadoId))
+	                .andExpect(status().isOk())
+	                .andExpect(jsonPath("$.idTurn").value(1))
+	                .andExpect(jsonPath("$.status").value("EN_ATENCION"));
+	    }
+}
---
+++ src/test/java/com/grupo12/SistemaTurnos/test/PasswordTest.java
@@ -0,0 +1,15 @@
+	package com.grupo12.test;
+	
+	import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+	
+	public class PasswordTest {
+		 public static void main(String[] args) {
+		        String encoded = "$2a$10$CV3MPIAjI5kBPM2084u1OHMGK/I/7OxviJDO2C0iDTJKffYGLNQ0q";
+		        String rawPassword = "admin123";
+	
+		        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		        boolean matches = encoder.matches(rawPassword, encoded);
+	
+		        System.out.println("¿Coincide? " + matches);
+		    }
+	}
