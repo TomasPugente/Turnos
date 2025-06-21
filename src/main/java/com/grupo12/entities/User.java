d7f1e4b0d3a06007a92fd3e6afb9f4e1ffd2d75f
maryse90 <106642662+maryse90@users.noreply.github.com>
2025-06-20 23:43:34 -0300

    Add files via upload

--- src/main/java/com/grupo12/entities/User.java
+++ src/main/java/com/grupo12/entities/User.java
@@ -6,25 +6,20 @@ import java.util.Set;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import lombok.Getter;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @RequiredArgsConstructor
-@Getter
-@Setter
+@Data
 @Table(name = "user")
 public class User {
 
@@ -34,18 +29,15 @@ public class User {
 
     @Column(name = "username", unique = true, nullable = false)
     private String username;
+    
+    @Column(name = "email", nullable = false)
+    private String email;
 
     @Column(name = "password", nullable = false)
     private String password;
 
     @Column(name = "enabled")
     private Boolean enabled;
-
-    @Column(name = "email", unique = true, nullable = false)
-    private String email;
-
-    @Column(name = "reset_token")
-    private String resetToken;
 
     @Column(name = "createdAt")
     @CreationTimestamp
@@ -54,10 +46,7 @@ public class User {
     @Column(name = "updatedAt")
     private LocalDateTime updatedAt;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "user")
     private Set<UserRole> userRoles = new HashSet<UserRole>();
 
-    @OneToOne
-    @JoinColumn(name = "person_id")
-    private Person person;
 }
